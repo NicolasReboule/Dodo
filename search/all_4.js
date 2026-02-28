eb373c37fdae1a0886f683567a211ea350f045c1
NicolasReboule <NicolasReboule@users.noreply.github.com>
2026-02-28 12:45:28 +0000

    deploy: c39c283c991b7367bb95cbb9e188acfe3e169dab

--- search/all_4.js
+++ search/all_4.js
@@ -1,11 +1,11 @@
 var searchData=
 [
   ['deallocate_0',['deallocate',['../structGLFWallocator.html#ab74cf9a969e73e6eb65a6112a591a988',1,'GLFWallocator']]],
-  ['debugcallback_1',['DebugCallback',['../classdodo_1_1core_1_1VulkanContext.html#ab0cb5569654f765b29ed6f37cc5ff01e',1,'dodo::core::VulkanContext']]],
-  ['debugcallback_2',['debugCallback',['../structdodo_1_1core_1_1VulkanContext_1_1VulkanDebug.html#a131a4c97a4c8eeb0e39e3de073db7c5f',1,'dodo::core::VulkanContext::VulkanDebug::debugCallback'],['../classHelloTriangleApplication.html#aa8eed5e54aaf3bcc49b9214a6e8c8e20',1,'HelloTriangleApplication::debugCallback()']]],
+  ['debugcallback_1',['DebugCallback',['../classdodo_1_1core_1_1VulkanContext.html#a8099f6355f3297fc635d14e3cb34f412',1,'dodo::core::VulkanContext']]],
+  ['debugcallback_2',['debugCallback',['../structdodo_1_1core_1_1VulkanContext_1_1VulkanDebug.html#a131a4c97a4c8eeb0e39e3de073db7c5f',1,'dodo::core::VulkanContext::VulkanDebug::debugCallback'],['../classHelloTriangleApplication.html#a255ceee02b1a75f5809d94bf87beb14b',1,'HelloTriangleApplication::debugCallback()']]],
   ['debugenabled_3',['debugEnabled',['../structdodo_1_1core_1_1VulkanContext_1_1VulkanDebug.html#a2555039d6f5bf29d540c2bf5d370672b',1,'dodo::core::VulkanContext::VulkanDebug']]],
   ['debuginfo_4',['debugInfo',['../structdodo_1_1core_1_1VulkanContext_1_1VulkanContextInfo.html#a03daf8cb9ffeebf54816fc17946af628',1,'dodo::core::VulkanContext::VulkanContextInfo']]],
-  ['defaultdebugcallback_5',['defaultDebugCallback',['../classdodo_1_1core_1_1VulkanContext.html#a429c94603a841becc52bc6152d2e4232',1,'dodo::core::VulkanContext']]],
+  ['defaultdebugcallback_5',['defaultDebugCallback',['../classdodo_1_1core_1_1VulkanContext.html#acece5bed58ddc63c71eccedee2ca4786',1,'dodo::core::VulkanContext']]],
   ['deprecated_20list_6',['Deprecated List',['../deprecated.html',1,'']]],
   ['deviceextensions_7',['deviceExtensions',['../classHelloTriangleApplication.html#acc044f0ec31c4eacf8e512087cf7743e',1,'HelloTriangleApplication']]],
   ['dodo_8',['dodo',['../namespacedodo.html',1,'']]],
--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['debugcallback_0',['DebugCallback',['../classdodo_1_1core_1_1VulkanContext.html#ab0cb5569654f765b29ed6f37cc5ff01e',1,'dodo::core::VulkanContext']]]
+  ['debugcallback_0',['DebugCallback',['../classdodo_1_1core_1_1VulkanContext.html#a8099f6355f3297fc635d14e3cb34f412',1,'dodo::core::VulkanContext']]]
 ];
